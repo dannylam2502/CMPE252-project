b21fd40dfe6c05fa933d55c1834d1433d913ac64
Anthony <JSnack58@gmail.com>
2025-03-13 14:42:24 -0700

    Cleanup and add comments explaining the functions in the code.

--- UnityProject/AILearn/Assets/Scenes/EnemyAgent/Scripts/EnemyAgent.cs
+++ UnityProject/AILearn/Assets/Scenes/EnemyAgent/Scripts/EnemyAgent.cs
@@ -7,18 +7,24 @@ using System;
 
 public class EnemyAgent : Agent
 {
+    // SerializeField are inputs accessible within Unity via the Inspector for the 
+    // Script Component.
+    
+    // What the Agent will chase.
     [SerializeField] private Transform _goal;
+    // Where the goal will spawn on.
     [SerializeField] private GameObject _ground;
+    // How the fast the agent will move and turn.
     [SerializeField] private float _moveSpeed = 1.5f;
     [SerializeField] private float _rotationSpeed = 180f;
-    //[SerializeField] private float _minRandomGoalLoc = -10f;
-    //[SerializeField] private float _maxRandomGoalLoc = 9.00f;
 
+    // Can make the Agent change color.
     private Renderer _renderer;
 
     private int _currentEpisode = 0;
     private float _cumulativeReward = 0f;
 
+    // Set for variables to render, track episodes, and rewards.
     public override void Initialize()
     {
         Debug.Log("Initialize()");
@@ -27,6 +33,7 @@ public class EnemyAgent : Agent
         _cumulativeReward = 0f;
     }
 
+    // Setup for each episode and place goal and agent.
     public override void OnEpisodeBegin()
     {
         Debug.Log("OnEpisodeBegin()");
@@ -37,6 +44,7 @@ public class EnemyAgent : Agent
         SpawnObjects();
     }
 
+    //  Randomly places the agent in a fixed place and the goal randomly on the ground.
     private void SpawnObjects()
     {
         transform.localRotation = Quaternion.identity;
@@ -46,7 +54,6 @@ public class EnemyAgent : Agent
         float randomAngle = UnityEngine.Random.Range(0f, 360f);
         Vector3 randomDirection = Quaternion.Euler(0f, randomAngle, 0f) * Vector3.forward;
 
-        //float randomDistance = UnityEngine.Random.Range(_minRandomGoalLoc, _maxRandomGoalLoc);
         float randomDistance = UnityEngine.Random.Range(1f, 9f);
         
         Vector3 goalPosition = transform.localPosition + randomDirection * randomDistance;
@@ -54,14 +61,14 @@ public class EnemyAgent : Agent
         _goal.localPosition = new Vector3(goalPosition.x, 0.62f, goalPosition.z);
         Debug.LogFormat("goalPos: {0}",_goal.localPosition);
     }
-    // Return the minimum normalized distance of the closest wall.
+
+    // [REMOVED] Return the minimum normalized distance of the closest wall.
     private float minWallProximity()
     {
         Collider[] proximity = new Collider[4];
         int radius = 5;
         float minNormDistance = 1;
         Physics.OverlapSphereNonAlloc(transform.position, radius, proximity);
-        //Collider[] proximity = Physics.OverlapSphere(transform.position, radius);
 
         for (int i = 0; i < proximity.Length; i++)
         {
@@ -70,14 +77,10 @@ public class EnemyAgent : Agent
                 continue;
             }
 
-            //Debug.LogFormat("tranform.position: {0}",transform.position);
             Vector3 closestPoint = proximity[i].ClosestPoint(transform.position);
-            //Debug.LogFormat("closestPoint: {0}", proximity[i].transform.position);
             float normDistance = Vector3.Distance(transform.position, proximity[i].transform.position) / radius;
-            //Debug.LogFormat("normDistance: {0}",normDistance);
             minNormDistance = Math.Min(minNormDistance, normDistance);
         }
-        //Debug.LogFormat("minNormDistance: {0}",minNormDistance);
         if (minNormDistance < 1)
         {
             AddReward(-0.01f * Time.fixedDeltaTime);
@@ -85,25 +88,17 @@ public class EnemyAgent : Agent
         return minNormDistance;
     }
 
+    // Collect the positions of the goal, and the agent orientation.
     public override void CollectObservations(VectorSensor sensor)
     {
-        //Debug.Log("CollectObservationos()");
         Bounds bounds = _ground.GetComponent<Collider>().bounds;
-        // float maxX = bounds.extents.x;
-        // float maxZ = bounds.extents.z;
         float goalPosX_normalized = _goal.localPosition.x / 5f;
         float goalPosZ_normalized = _goal.localPosition.z / 5f;
-        // float goalPosX_normalized = _goal.localPosition.normalized.x;
-        // float goalPosZ_normalized = _goal.localPosition.normalized.z;
 
         float enemyPosX_normalized = transform.localPosition.x / 5f;
         float enemyPosZ_normalized = transform.localPosition.z / 5f;
-        // float enemyPosX_normalized = transform.localPosition.normalized.x;
-        // float enemyPosZ_normalized = transform.localPosition.normalized.z;
-        //Debug.LogFormat("goalX={0}, maxX={1}, goalPosX_normalized={2}", _goal.localPosition.x, maxX, goalPosX_normalized);
         float enemyRotation_normalized = (transform.localRotation.eulerAngles.y / 360f) * 2f - 1f;
-        //float wallProximity_normalized = minWallProximity();
-        sensor.AddObservation(goalPosX_normalized);
+        //float wallProximity_normalized = minWallProximity();rvation(goalPosX_normalized);
         sensor.AddObservation(goalPosZ_normalized);
         sensor.AddObservation(enemyPosX_normalized);
         sensor.AddObservation(enemyPosZ_normalized);
@@ -111,26 +106,21 @@ public class EnemyAgent : Agent
         //sensor.AddObservation(wallProximity_normalized);
     }
 
+    // Move the agent and reward it for taking the action.
     public override void OnActionReceived(ActionBuffers actions)
     {
         MoveAgent(actions.DiscreteActions);
 
         AddReward(-2f / MaxStep);
-        // Treating the positions as vectors, normalize the vectors so their length is one.
-        // float distanceToTarget = Vector3.Distance(_goal.transform.localPosition, transform.localPosition);
-        // AddReward(-Math.Abs(distanceToTarget)/MaxStep);
-        // if (distance > 0){
-        //     // AddReward(-0.01f * Math.Abs(distance));
-        //     // The max distance between the vectors is if they're pointing in opposite directions: 2
-        //     AddReward(-Math.Abs(distance)/MaxStep);
-        // }
 
         _cumulativeReward = GetCumulativeReward();
 
     }
 
+    // Move the agent based on its orientation and movement speed.
     public void MoveAgent(ActionSegment<int> act)
     {
+        // From the sampled action from the ActionSegement will be numbers 1 to 3.
         var action = act[0];
         switch (action)
         {
@@ -146,15 +136,17 @@ public class EnemyAgent : Agent
         }
     }
 
+    // Set Goal Trigger when goal is touched.
     public void OnTriggerEnter(Collider other)
     {
-        // if (other.gameObject.transform.parent.CompareTag("target"))
         if (other.gameObject.CompareTag("target"))
         {
             Debug.Log("Goal Reached");
             GoalReached();
         }
     }
+    
+    // Reward the agent for reaching the goal and end the episode for the next on.
     private void GoalReached()
     {
         AddReward(1.0f);
@@ -167,7 +159,8 @@ public class EnemyAgent : Agent
             component.OnHitByEnemy(this.gameObject);
         }
     }
-
+    
+    // Penalize the agent and color it red for touching the walls.
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("wall"))
@@ -179,7 +172,8 @@ public class EnemyAgent : Agent
             }
         }
     }
-
+    
+    // Penalize the agent for sustained contact with walls.
     private void OnCollisionStay(Collision collision)
     {
         if ( collision.gameObject.CompareTag("wall"))
@@ -187,7 +181,8 @@ public class EnemyAgent : Agent
             AddReward(-0.01f * Time.fixedDeltaTime);
         }
     }
-
+    
+    // Return the agent to blue when it stops touching walls.
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.CompareTag("wall"))
